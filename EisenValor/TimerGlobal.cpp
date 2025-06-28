bf5e07ca406b788fa8751b7f58f9e19183250a3e
이윤상 <kwjdduf88@gmail.com>
2025-06-29 03:39:28 +0900

    refactor: restructure global system and integrate DirectX - Updated registry system, renamed members, and extended input/timer

--- EisenValor/TimerGlobal.cpp
+++ EisenValor/TimerGlobal.cpp
@@ -3,8 +3,43 @@
 
 void TimerGlobal::Initialize()
 {
+	QueryPerformanceFrequency(&m_CpuFrequency);
+	QueryPerformanceCounter(&m_PrevFrequency);
 }
 
 void TimerGlobal::Update()
 {
+	QueryPerformanceCounter(&m_CurFrequency);
+	float diffFrequency = static_cast<float>(m_CurFrequency.QuadPart - m_PrevFrequency.QuadPart);
+	m_DeltaTime = diffFrequency / static_cast<float>(m_CpuFrequency.QuadPart);
+
+	if (m_TargetFPS > 0)
+	{
+		float minFrameTime = 1.0f / m_TargetFPS;
+		while (m_DeltaTime < minFrameTime - 0.002f)
+		{
+			Sleep(0);
+			QueryPerformanceCounter(&m_CurFrequency);
+			diffFrequency = static_cast<float>(m_CurFrequency.QuadPart - m_PrevFrequency.QuadPart);
+			m_DeltaTime = diffFrequency / static_cast<float>(m_CpuFrequency.QuadPart);
+		}
+		while (m_DeltaTime < minFrameTime)
+		{
+			QueryPerformanceCounter(&m_CurFrequency);
+			diffFrequency = static_cast<float>(m_CurFrequency.QuadPart - m_PrevFrequency.QuadPart);
+			m_DeltaTime = diffFrequency / static_cast<float>(m_CpuFrequency.QuadPart);
+		}
+	}
+	m_PrevFrequency.QuadPart = m_CurFrequency.QuadPart;
+	m_RunTime += m_DeltaTime;
+	m_accumulator += m_DeltaTime;
+
+	m_FrameCount++;
+	m_CurTime += m_DeltaTime;
+	if (m_CurTime > 1.0f)
+	{
+		m_CurFPS = m_FrameCount;
+		m_FrameCount = 0;
+		m_CurTime = 0;
+	}
 }
--- EisenValor/TimerGlobal.h
+++ EisenValor/TimerGlobal.h
@@ -2,9 +2,41 @@
 #include "ITimerGlobal.h"
 
 class TimerGlobal : public GlobalMakerBase<TimerGlobal, ITimerGlobal> {
-
 public:
-	void Initialize() override;
-	void Update() override;
-};
+	void Initialize() final;
+	void Update() final;
+	inline bool ShouldFixedUpdate() noexcept final
+	{
+		if (m_FixedDeltaTime > m_accumulator) return false;
 
+		m_accumulator -= m_FixedDeltaTime;
+		return true;
+	}
+
+	[[nodiscard]]
+	inline float GetDeltaTime() const noexcept final { return m_DeltaTime; }
+	[[nodiscard]]
+	inline float GetFixedDeltaTime() const noexcept final { return m_FixedDeltaTime; }
+	inline void SetTargetFPS(uint32_t targetFPS) noexcept final { m_TargetFPS = targetFPS; }
+	inline void SetFixedFPS(uint32_t fixedFPS) noexcept final { m_FixedDeltaTime = 1.0f / fixedFPS; }
+
+	inline float GetRuntime() const noexcept final { return m_RunTime; }
+	inline float GetFPS() const noexcept final { return m_CurFPS; }
+
+private:
+	LARGE_INTEGER m_CpuFrequency;
+	LARGE_INTEGER m_PrevFrequency;
+	LARGE_INTEGER m_CurFrequency;
+
+	float m_RunTime = 0.0f;
+	float m_accumulator = 0.0f;
+
+	float m_DeltaTime = 0.0f;
+	float m_FixedDeltaTime = 0.032f;
+	uint32_t m_CurFPS = 0;
+
+	float m_CurTime = 0.0f;
+	uint32_t m_FrameCount = 0;
+
+	uint32_t m_TargetFPS = 0;
+};
