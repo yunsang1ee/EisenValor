9e45497d20974e44a3be95a88b0177d651ca3eee
Seongyeol <syyundev@gmail.com>
2025-07-02 23:08:39 +0900

    refactor: 더미 클라이언트 이동
    Server 프로젝트에 있던 더미 클라이언트 분리

---
+++ EisenValor-DummyClient/DummyClient/DummyClient.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+
+int main()
+{
+	std::wcout.imbue(std::locale("korean"));
+
+	WSADATA wsaData;
+	if(0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
+		std::cout << "WSAStartup Failed!" << std::endl;
+		return -1;
+	}
+
+	const SOCKET clientSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
+
+	if(clientSocket == INVALID_SOCKET) {
+		const int errCode = WSAGetLastError();
+		std::cout << errCode << std::endl;
+	}
+
+	unsigned long noblock = 1;
+	int nRet = ioctlsocket(clientSocket, FIONBIO, &noblock);
+
+	const std::string_view ip{ "127.0.0.1" };
+
+	SOCKADDR_IN addr{};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(7777);
+	inet_pton(AF_INET, ip.data(), &addr.sin_addr);
+	if(SOCKET_ERROR == WSAConnect(clientSocket, reinterpret_cast<const sockaddr*>(&addr), sizeof(SOCKADDR_IN), NULL, NULL, NULL, NULL)) {
+		int errCode = ::WSAGetLastError();
+		if(errCode == WSAEWOULDBLOCK)
+			std::cout << "WSAConnect WSAEWOULDBLOCK!" << std::endl;
+		else {
+			std::cout << "WSAConnect Failed!" << std::endl;
+			return -1;
+		}
+	}
+
+	std::cout << "Server Connection Success!" << std::endl;
+
+	while(true) {
+		std::string str;
+		std::cin >> str;
+		CS_CHAT_PACKET sendPkt;
+		sendPkt.packetSize = sizeof(sendPkt);
+		sendPkt.packetType = static_cast<uint16>(PACKET_TYPE::CS_CHAT);
+		memcpy(sendPkt.msg, str.data(), str.size());
+		sendPkt.msg[static_cast<int32>(str.size())] = 0;
+		send(clientSocket, (const char*)(&sendPkt), sizeof(sendPkt), 0);
+	}
+
+}
---
+++ EisenValor-DummyClient/DummyClient/pch.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+
+#include <iostream> 
+#include <string>
+#include <string_view>
+
+#include "../../EisenValor-Server/ServerEngine/ServerEnginePch.h"
+#include "../../EisenValor-Server/Server/TestPackets.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "../../EisenValor-Server/Libraries/lib/ServerEngine/Debug/ServerEngine_Debug.lib")
+#else
+#pragma comment(lib, "../../EisenValor-Server/Libraries/lib/ServerEngine/Release/ServerEngine_Release.lib")
+#endif
