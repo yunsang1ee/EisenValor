35b1a2be2768a423baa247e0ea0ddeda2899bef9
Seongyeol <syyundev@gmail.com>
2025-07-07 00:25:37 +0900

    feat: flatbuffer 라이브러리 추가
    빌드 전 GenPackets.bat이 실행됩니다.
    Genpackets.bat 안에선 flatbuffer schema코드가 컴파일 되어 결과물이 헤더파일로 출력됩니다.

--- EisenValor-DummyClient/DummyClient/DummyClient.cpp
+++ EisenValor-DummyClient/DummyClient/DummyClient.cpp
@@ -1,52 +1,84 @@
 #include "pch.h"
+#include "SendBuffer.h"
+
+//void DoRecv(const SOCKET& clientSocket)
+//{
+//	char szBuffer[128]{};
+//	while(true) {
+//		int recvLen = ::recv(clientSocket, szBuffer, sizeof(szBuffer), 0);
+//		if(recvLen > 0) {
+//
+//			const PacketHeader packetHeader = *reinterpret_cast<const PacketHeader*>(szBuffer);
+//			std::println("Packet Size: {}", packetHeader.packetSize);
+//			std::println("Packet Type: {}", packetHeader.packetType);
+//
+//			const char* packetData = buffer + sizeof(PacketHeader);
+//
+//			NetBridge::ServerPacketHandler::HandlePacket(clientSocket, packetData, packetHeader);
+//
+//		}
+//		else {
+//			int err = WSAGetLastError();
+//			if(recvLen == 0) {
+//				// 연결 종료
+//				printf("Server closed connection\n");
+//				break;
+//			}
+//			else if(err == WSAEWOULDBLOCK) {
+//				// 수신할 데이터 없음
+//				std::this_thread::sleep_for(1ms);  // busy-loop 방지
+//				continue;
+//			}
+//			else {
+//				// 기타 에러
+//				printf("recv failed, err=%d\n", err);
+//				break;
+//			}
+//		}
+//	}
+//}
+std::atomic_bool EXIT_FLAG = false;
 
 int main()
 {
-	std::wcout.imbue(std::locale("korean"));
+	NetBridge::ServerPacketHandler::Init();
 
-	WSADATA wsaData;
-	if(0 != WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-		std::cout << "WSAStartup Failed!" << std::endl;
+	std::string ip;
+	uint16 port;
+
+	std::cout << "IP 입력:";
+	std::cin >> ip;
+	std::cout << "PORT 입력:";
+	std::cin >> port;
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // ← 여기 추가
+
+	if(false ==	MANAGER(NetBridge::NetworkManager)->Init(ip, port)) {
+		std::cout << "NetworkManager Init Failed!" << std::endl;
 		return -1;
 	}
-
-	const SOCKET clientSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, WSA_FLAG_OVERLAPPED);
-
-	if(clientSocket == INVALID_SOCKET) {
-		const int errCode = WSAGetLastError();
-		std::cout << errCode << std::endl;
-	}
-
-	unsigned long noblock = 1;
-	int nRet = ioctlsocket(clientSocket, FIONBIO, &noblock);
-
-	const std::string_view ip{ "127.0.0.1" };
-
-	SOCKADDR_IN addr{};
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(7777);
-	inet_pton(AF_INET, ip.data(), &addr.sin_addr);
-	if(SOCKET_ERROR == WSAConnect(clientSocket, reinterpret_cast<const sockaddr*>(&addr), sizeof(SOCKADDR_IN), NULL, NULL, NULL, NULL)) {
-		int errCode = ::WSAGetLastError();
-		if(errCode == WSAEWOULDBLOCK)
-			std::cout << "WSAConnect WSAEWOULDBLOCK!" << std::endl;
-		else {
-			std::cout << "WSAConnect Failed!" << std::endl;
-			return -1;
-		}
-	}
-
+	
 	std::cout << "Server Connection Success!" << std::endl;
+
+	std::thread t{ []() {
+		while(false ==  EXIT_FLAG) {
+		MANAGER(NetBridge::NetworkManager)->ProcessIO();
+		}
+	} };
 
 	while(true) {
 		std::string str;
 		std::getline(std::cin, str); 
-		CS_CHAT_PACKET sendPkt;
-		sendPkt.packetSize = sizeof(sendPkt);
-		sendPkt.packetType = static_cast<uint16>(PACKET_TYPE::CS_CHAT);
-		memcpy(sendPkt.msg, str.data(), str.size());
-		sendPkt.msg[str.size()] = 0;
-		send(clientSocket, (const char*)(&sendPkt), sizeof(sendPkt), 0);
+		
+		if(str == "EXIT")
+			break;
+		
+		const auto packetData = NetBridge::ServerPacketHandler::Make_CS_CHAT_PACKET(str.c_str());
+		auto sendBuffer = NetBridge::ServerPacketHandler::MakeSendBuffer(PACKET_TYPE::CS_CHAT, packetData);
+		SendPacket(std::move(sendBuffer));
 	}
-
+	
+	EXIT_FLAG = true;
+	t.join();
+	MANAGER(NetBridge::NetworkManager)->Terminate();
 }
--- EisenValor-DummyClient/DummyClient/pch.h
+++ EisenValor-DummyClient/DummyClient/pch.h
@@ -1,20 +1,9 @@
 ﻿#pragma once
 
-#include <WS2tcpip.h>
-#include <MSWSock.h>
-
-#pragma comment(lib, "WS2_32.lib")
-#pragma comment(lib, "MSWSock.lib")
-
-#include <iostream> 
-#include <string>
-#include <string_view>
-
-#include "../../EisenValor-Server/ServerEngine/ServerEnginePch.h"
-#include "../../EisenValor-Server/Server/TestPackets.h"
-
 #ifdef _DEBUG
-#pragma comment(lib, "../../EisenValor-Server/Libraries/lib/ServerEngine/Debug/ServerEngine_Debug.lib")
+#pragma comment(lib, "NetBridge/Debug/NetBridge_Debug.lib")
 #else
-#pragma comment(lib, "../../EisenValor-Server/Libraries/lib/ServerEngine/Release/ServerEngine_Release.lib")
+#pragma comment(lib, "NetBridge/Release/NetBridge_Release.lib")
 #endif
+
+#include "NetBridgePch.h"
