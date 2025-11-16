705f92c9afde74733bf8ef3ddc6354cd4b7f6ce7
Seongyeol Yun <syyundev@gmail.com>
2025-11-16 22:35:28 +0900

    fix

--- EisenValor-Server/Server/Creature.cpp
+++ EisenValor-Server/Server/Creature.cpp
@@ -20,8 +20,3 @@ void Server::Contents::Creature::SetHp(const int32 hp) noexcept
 		m_stat.hp = std::max(0, hp);
 	}
 }
-
-void Server::Contents::Creature::OnDamaged(std::shared_ptr<Creature> attacker, const int32 damaged)
-{
-
-}
