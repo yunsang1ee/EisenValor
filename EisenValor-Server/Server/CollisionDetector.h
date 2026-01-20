bb689ddd3de1524378b593fed01186c95b37cbd2
Seongyeol Yun <syyundev@gmail.com>
2026-01-20 21:46:06 +0900

    feat: 충돌체 구현

---
+++ EisenValor-Server/Server/CollisionDetector.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Collider.h"
+
+namespace Server {
+	namespace Contents {
+
+		class GameWorld;
+
+		class Collider;
+		class OBBCollider;
+
+		class CollisionDetector {
+		private:
+			using CollisionFunc = bool(*)(const Collider* const, const Collider* const);
+			static constexpr uint8 MAX_TYPE_COUNT = static_cast<int>(COLLIDER_TYPE::END);
+		
+		private:
+			CollisionFunc m_collisionTable[MAX_TYPE_COUNT + 1][MAX_TYPE_COUNT + 1];
+
+		private:
+			CollisionDetector();
+			CollisionDetector(const CollisionDetector&) = delete;
+			CollisionDetector(CollisionDetector&&) = delete;
+			CollisionDetector& operator=(const CollisionDetector&) = delete;
+			CollisionDetector& operator=(CollisionDetector&&) = delete;
+
+		public:
+			bool CheckCollision(const Collider* const left, const Collider* const right);
+
+		private:
+			void Init();
+		
+		private:
+			static bool Check_OBB_OBB(const Collider* const left, const Collider* const right);
+
+		private:
+			static bool OverlapOnAxis(const OBBCollider* const box1, const OBBCollider* const box2, const Matrix& mat1, const Matrix& mat2, const Vec3& axis);
+		
+			friend class GameWorld;
+		};
+	}
+}
