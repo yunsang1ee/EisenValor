d186c5f4f2e3aa432f7bbd139dd4fc599253050d
Seongyeol Yun <syyundev@gmail.com>
2026-02-18 00:19:54 +0900

    Remove: 정육면체 임시 제거

--- EisenValor-Client/EisenValor/Scene/SampleScene.cpp
+++ EisenValor-Client/EisenValor/Scene/SampleScene.cpp
@@ -155,7 +155,7 @@ void SampleScene::CreateSceneObjects()
 		}
 	);
 
-	DX::XMFLOAT3 positions[3] = {{-4.0f, 3.0f, 0.0f}, {0.0f, 1.0f, 5.0f}, {4.0f, 3.0f, 0.0f}};
+	/*DX::XMFLOAT3 positions[3] = {{-4.0f, 3.0f, 0.0f}, {0.0f, 1.0f, 5.0f}, {4.0f, 3.0f, 0.0f}};
 
 	DX::XMFLOAT3 rotations[3] = {{10.0f, 15.0f, 0.0f}, {-5.0f, 120.0f, 3.0f}, {8.0f, 210.0f, -10.0f}};
 
@@ -176,7 +176,7 @@ void SampleScene::CreateSceneObjects()
 				);
 			}
 		);
-	}
+	}*/
 	DEBUG_LOG_FMT("[SampleScene] Created {} GameObjects\n", 8);
 }
 
