adfd74fd99805050298234b240228c523aaabac7
dageonlee@gameengineerning <leedageon764@gmail.com>
2026-03-08 00:20:38 +0900

    feat: Login Scene

---
+++ EisenValor-Client/EisenValor/Component/Login/LoginControllerComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <IComponent.h>
+
+class LoginControllerComponent final : public ComponentBase<LoginControllerComponent>
+{
+public:
+	static constexpr const char* GetStaticTypeName() { return "LoginControllerComponent"; }
+
+	void OnUpdate(float deltaTime);
+
+private:
+	bool m_isLoginRequested = false;
+};
