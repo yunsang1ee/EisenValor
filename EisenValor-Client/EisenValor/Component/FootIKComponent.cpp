6a01c356a679d8ad8a36982e864f37665bf34e9d
da-geonLee <leedageon764@gmail.com>
2026-06-06 23:05:31 +0900

    feat: 필요없는 코드 삭제

--- EisenValor-Client/EisenValor/Component/FootIKComponent.cpp
+++ EisenValor-Client/EisenValor/Component/FootIKComponent.cpp
@@ -192,281 +192,6 @@ void FootIKComponent::OnLateUpdate(float deltaTime)
 		return;
 	}
 
-	static std::unordered_set<const FootIKComponent*> loggedComponents;
-	if (loggedComponents.insert(this).second)
-	{
-		auto&		ownerTransform = owner->GetTransform();
-		const auto  ownerWorldPosition = ownerTransform.GetWorldPosition();
-		const auto  leftFootWorld = TransformBonePositionToWorld(leftFootMatrix.r[3], ownerTransform);
-		const auto  leftTargetWorld = TransformBonePositionToWorld(leftTargetMatrix.r[3], ownerTransform);
-		const auto  rightFootWorld = TransformBonePositionToWorld(rightFootMatrix.r[3], ownerTransform);
-		const auto  rightTargetWorld = TransformBonePositionToWorld(rightTargetMatrix.r[3], ownerTransform);
-		DirectX::XMFLOAT3 leftFootWorldPosition;
-		DirectX::XMFLOAT3 rightFootWorldPosition;
-		DirectX::XMStoreFloat3(&leftFootWorldPosition, leftFootWorld);
-		DirectX::XMStoreFloat3(&rightFootWorldPosition, rightFootWorld);
-		const DirectX::XMFLOAT3 footCenter = {
-			(leftFootWorldPosition.x + rightFootWorldPosition.x) * 0.5f,
-			(leftFootWorldPosition.y + rightFootWorldPosition.y) * 0.5f,
-			(leftFootWorldPosition.z + rightFootWorldPosition.z) * 0.5f
-		};
-
-		/*DEBUG_LOG_FMT(
-			"[FootIK] modelY leftFoot={:.3f}, leftIkFoot={:.3f}, rightFoot={:.3f}, rightIkFoot={:.3f}\n",
-			DirectX::XMVectorGetY(leftFootMatrix.r[3]), DirectX::XMVectorGetY(leftTargetMatrix.r[3]),
-			DirectX::XMVectorGetY(rightFootMatrix.r[3]), DirectX::XMVectorGetY(rightTargetMatrix.r[3])
-		);
-		DEBUG_LOG_FMT(
-			"[FootIK] worldY player={:.3f}, leftFoot={:.3f}, leftIkFoot={:.3f}, rightFoot={:.3f}, rightIkFoot={:.3f}\n",
-			ownerWorldPosition.y, DirectX::XMVectorGetY(leftFootWorld), DirectX::XMVectorGetY(leftTargetWorld),
-			DirectX::XMVectorGetY(rightFootWorld), DirectX::XMVectorGetY(rightTargetWorld)
-		);*/
-
-		if (auto* scene = owner->GetScene())
-		{
-			if (auto* meshStorage = scene->GetStorage<MeshComponent>())
-			{
-				//DEBUG_LOG_FMT("[FootIK] scene mesh count={}\n", meshStorage->GetList().size());
-
-				constexpr float nearbyRadius = 5.0f;
-				constexpr float nearbyRadiusSq = nearbyRadius * nearbyRadius;
-				constexpr float nearbyHeightTolerance = 0.5f;
-				size_t nearbyCandidateCount = 0;
-				size_t nearbyHeightCandidateCount = 0;
-				size_t loggedCandidateCount = 0;
-				size_t loggedHeightCandidateCount = 0;
-				size_t loggedMeshCount = 0;
-				struct HeightCandidate
-				{
-					const GameObject* obj = nullptr;
-					const MeshResource* res = nullptr;
-					DirectX::XMFLOAT3 worldPos = {};
-					float distanceSqXZ = 0.0f;
-					float deltaY = 0.0f;
-				};
-				std::vector<HeightCandidate> heightCandidates;
-				std::unordered_set<EvAsset::Guid, EvAsset::GuidHash> uniqueHeightCandidateGuids;
-				const GameObject* closestHeightCandidateObj = nullptr;
-				const MeshResource* closestHeightCandidateRes = nullptr;
-				DirectX::XMFLOAT3 closestHeightCandidateWorld = {};
-				float closestHeightCandidateDistanceSq = std::numeric_limits<float>::max();
-				float closestHeightCandidateDeltaY = 0.0f;
-				for (const auto& meshComp : meshStorage->GetList())
-				{
-					if (!meshComp.IsValid())
-					{
-						continue;
-					}
-
-					auto* meshObj = meshComp.GetGameObject();
-					auto* meshRes = meshComp.GetMeshResource();
-					if (!meshObj || !meshRes)
-					{
-						continue;
-					}
-
-					const auto worldPos = meshObj->GetTransform().GetWorldPosition();
-					if (loggedMeshCount < 12)
-					{
-						//DEBUG_LOG_FMT(
-						//	"[FootIK] scene mesh obj='{}' guid={} world=({:.3f}, {:.3f}, {:.3f})\n",
-						//	meshObj->GetName().c_str(), meshRes->GetGuid(), worldPos.x, worldPos.y, worldPos.z
-						//);
-						++loggedMeshCount;
-					}
-
-					const float distanceSqXZ = DistanceSqXZ(worldPos, footCenter);
-					if (distanceSqXZ <= nearbyRadiusSq)
-					{
-						++nearbyCandidateCount;
-
-						if (loggedCandidateCount < 8)
-						{
-							//DEBUG_LOG_FMT(
-							//	"[FootIK] nearby mesh candidate obj='{}' guid={} world=({:.3f}, {:.3f}, {:.3f}) distXZ={:.3f}\n",
-							//	meshObj->GetName().c_str(), meshRes->GetGuid(), worldPos.x, worldPos.y, worldPos.z,
-							//	std::sqrt(distanceSqXZ)
-							//);
-							++loggedCandidateCount;
-						}
-
-						const float heightDelta = std::fabs(worldPos.y - footCenter.y);
-						if (heightDelta <= nearbyHeightTolerance)
-						{
-							++nearbyHeightCandidateCount;
-							uniqueHeightCandidateGuids.insert(meshRes->GetGuid());
-							heightCandidates.push_back({meshObj, meshRes, worldPos, distanceSqXZ, heightDelta});
-
-							if (distanceSqXZ < closestHeightCandidateDistanceSq)
-							{
-								closestHeightCandidateObj = meshObj;
-								closestHeightCandidateRes = meshRes;
-								closestHeightCandidateWorld = worldPos;
-								closestHeightCandidateDistanceSq = distanceSqXZ;
-								closestHeightCandidateDeltaY = heightDelta;
-							}
-
-							if (loggedHeightCandidateCount < 8)
-							{
-								DEBUG_LOG_FMT(
-									"[FootIK] nearby height candidate obj='{}' guid={} world=({:.3f}, {:.3f}, {:.3f}) distXZ={:.3f} deltaY={:.3f}\n",
-									meshObj->GetName().c_str(), meshRes->GetGuid(), worldPos.x, worldPos.y, worldPos.z,
-									std::sqrt(distanceSqXZ), heightDelta
-								);
-								++loggedHeightCandidateCount;
-							}
-						}
-					}
-
-				}
-
-				DEBUG_LOG_FMT(
-					"[FootIK] nearby mesh candidate count={} radius={:.1f} footCenter=({:.3f}, {:.3f}, {:.3f})\n",
-					nearbyCandidateCount, nearbyRadius, footCenter.x, footCenter.y, footCenter.z
-				);
-				DEBUG_LOG_FMT(
-					"[FootIK] nearby height candidate count={} uniqueGuidCount={} radius={:.1f} heightTolerance={:.1f}\n",
-					nearbyHeightCandidateCount, uniqueHeightCandidateGuids.size(), nearbyRadius, nearbyHeightTolerance
-				);
-				if (closestHeightCandidateObj && closestHeightCandidateRes)
-				{
-					std::filesystem::path closestHeightCandidatePath;
-					DEBUG_LOG_FMT(
-						"[FootIK] closest height candidate obj='{}' guid={} world=({:.3f}, {:.3f}, {:.3f}) distXZ={:.3f} deltaY={:.3f}\n",
-						closestHeightCandidateObj->GetName().c_str(), closestHeightCandidateRes->GetGuid(),
-						closestHeightCandidateWorld.x, closestHeightCandidateWorld.y, closestHeightCandidateWorld.z,
-						std::sqrt(closestHeightCandidateDistanceSq), closestHeightCandidateDeltaY
-					);
-					if (GLOBAL(ResourceGlobal).TryGetPath(closestHeightCandidateRes->GetGuid(), closestHeightCandidatePath))
-					{
-						DEBUG_LOG_FMT(
-							"[FootIK] closest height candidate path={}\n", closestHeightCandidatePath.string()
-						);
-
-						EvAsset::MeshData meshData;
-						if (EvAsset::AssetLoader::Load(closestHeightCandidatePath, meshData))
-						{
-							DEBUG_LOG_FMT(
-								"[FootIK] closest height mesh loaded vertices={} indices={} subMeshes={}\n",
-								meshData.vertices.size(), meshData.indices.size(), meshData.subMeshes.size()
-							);
-						}
-						else
-						{
-							DEBUG_LOG_FMT("[FootIK] closest height mesh load failed\n");
-						}
-					}
-					else
-					{
-						DEBUG_LOG_FMT("[FootIK] closest height candidate path lookup failed\n");
-					}
-				}
-
-				// Ray를 이용한 충돌 검사로 후보군의 실제 높이 샘플링
-				const auto rayOrigin = DirectX::XMVectorSet(footCenter.x, footCenter.y + 0.5f, footCenter.z, 1.0f);
-				const auto rayDir = DirectX::XMVectorSet(0.0f, -1.0f, 0.0f, 0.0f);
-				constexpr float rayMaxDistance = 1.0f;
-				float bestRayDistance = std::numeric_limits<float>::max();
-				DirectX::XMFLOAT3 bestHitPoint = {};
-				const HeightCandidate* bestRayCandidate = nullptr;
-				size_t rayCheckedCandidateCount = 0;
-				size_t rayLoadedCandidateCount = 0;
-
-				for (const auto& candidate : heightCandidates)
-				{
-					if (!candidate.obj || !candidate.res)
-					{
-						continue;
-					}
-
-					++rayCheckedCandidateCount;
-					std::filesystem::path candidatePath;
-					if (!GLOBAL(ResourceGlobal).TryGetPath(candidate.res->GetGuid(), candidatePath))
-					{
-						continue;
-					}
-
-					EvAsset::MeshData meshData;
-					if (!EvAsset::AssetLoader::Load(candidatePath, meshData))
-					{
-						continue;
-					}
-					++rayLoadedCandidateCount;
-
-					const auto worldMatrix = candidate.obj->GetTransform().GetWorldMatrix();
-					const auto world = DirectX::XMLoadFloat4x4(&worldMatrix);
-
-					for (size_t index = 0; index + 2 < meshData.indices.size(); index += 3)
-					{
-						const uint32_t i0 = meshData.indices[index];
-						const uint32_t i1 = meshData.indices[index + 1];
-						const uint32_t i2 = meshData.indices[index + 2];
-						if (i0 >= meshData.vertices.size() || i1 >= meshData.vertices.size() || i2 >= meshData.vertices.size())
-						{
-							continue;
-						}
-
-						const auto v0 = DirectX::XMVector3TransformCoord(
-							DirectX::XMVectorSet(
-								meshData.vertices[i0].position[0], meshData.vertices[i0].position[1],
-								meshData.vertices[i0].position[2], 1.0f
-							),
-							world
-						);
-						const auto v1 = DirectX::XMVector3TransformCoord(
-							DirectX::XMVectorSet(
-								meshData.vertices[i1].position[0], meshData.vertices[i1].position[1],
-								meshData.vertices[i1].position[2], 1.0f
-							),
-							world
-						);
-						const auto v2 = DirectX::XMVector3TransformCoord(
-							DirectX::XMVectorSet(
-								meshData.vertices[i2].position[0], meshData.vertices[i2].position[1],
-								meshData.vertices[i2].position[2], 1.0f
-							),
-							world
-						);
-
-						float distance = 0.0f;
-						if (!DirectX::TriangleTests::Intersects(rayOrigin, rayDir, v0, v1, v2, distance))
-						{
-							continue;
-						}
-
-						if (distance < 0.0f || distance > rayMaxDistance || distance >= bestRayDistance)
-						{
-							continue;
-						}
-
-						bestRayDistance = distance;
-						bestRayCandidate = &candidate;
-						const auto hitPoint =
-							DirectX::XMVectorMultiplyAdd(rayDir, DirectX::XMVectorReplicate(distance), rayOrigin);
-						DirectX::XMStoreFloat3(&bestHitPoint, hitPoint);
-					}
-				}
-
-				DEBUG_LOG_FMT(
-					"[FootIK] raycast scan checkedCandidates={} loadedMeshes={} maxDistance={:.2f}\n",
-					rayCheckedCandidateCount, rayLoadedCandidateCount, rayMaxDistance
-				);
-				if (bestRayCandidate)
-				{
-					DEBUG_LOG_FMT(
-						"[FootIK] raycast best hit obj='{}' guid={} hit=({:.3f}, {:.3f}, {:.3f}) distance={:.3f} candidateDistXZ={:.3f} candidateDeltaY={:.3f}\n",
-						bestRayCandidate->obj->GetName().c_str(), bestRayCandidate->res->GetGuid(), bestHitPoint.x,
-						bestHitPoint.y, bestHitPoint.z, bestRayDistance, std::sqrt(bestRayCandidate->distanceSqXZ),
-						bestRayCandidate->deltaY
-					);
-				}
-				else
-				{
-					DEBUG_LOG_FMT("[FootIK] raycast best hit not found\n");
-				}
-			}
-		}
-	}
 
 	// 발 위치를 월드 좌표로 변환
 	auto&	   ownerTransform = owner->GetTransform();
@@ -583,10 +308,11 @@ void FootIKComponent::OnLateUpdate(float deltaTime)
 	m_leftWeight = SmoothApproach(m_leftWeight, leftPelvisSupport ? 1.0f : 0.0f, deltaTime, 18.0f);
 	m_rightWeight = SmoothApproach(m_rightWeight, rightPelvisSupport ? 1.0f : 0.0f, deltaTime, 18.0f);
 
-	// 각 발의 Pole Vector를 계산
+	// 원본 애니메이션의 무릎 방향을 힌트로 넘기기
 	const auto leftPoleVector = DirectX::XMVector3Normalize(DirectX::XMVectorSubtract(leftCalfMatrix.r[3], leftThighMatrix.r[3]));
 	const auto rightPoleVector =
 		DirectX::XMVector3Normalize(DirectX::XMVectorSubtract(rightCalfMatrix.r[3], rightThighMatrix.r[3]));
+
 	auto buildLegIKTarget = [](const LegBoneCache& cache, DirectX::FXMVECTOR targetPos, DirectX::FXMVECTOR poleVector, float weight)
 	{
 		IKTarget target;
@@ -795,21 +521,21 @@ bool FootIKComponent::TrySampleVisualGround(
 	outHit.distance = bestRayDistance;
 	UpdateHitMarker(this, scene, bestHitPoint);
 	static uint32_t hitLogCounter = 0;
-	if (bestHitObj && bestHitRes && (++hitLogCounter % 30) == 0)
-	{
-		std::filesystem::path meshPath;
-		const bool hasPath = GLOBAL(ResourceGlobal).TryGetPath(bestHitRes->GetGuid(), meshPath);
-		DEBUG_LOG_FMT(
-			"[FootIK] sample hit obj='{}' guid={} path='{}' foot=({:.3f},{:.3f},{:.3f}) ground=({:.3f},{:.3f},{:.3f}) distance={:.3f}\n",
-			bestHitObj->GetName().c_str(), bestHitRes->GetGuid(), hasPath ? meshPath.string() : "<unresolved>",
-			worldPosition.x, worldPosition.y, worldPosition.z, bestHitPoint.x, bestHitPoint.y, bestHitPoint.z,
-			bestRayDistance
-		);
-		DEBUG_LOG_FMT(
-			"[FootIK] hit tri v0=({:.3f},{:.3f},{:.3f}) v1=({:.3f},{:.3f},{:.3f}) v2=({:.3f},{:.3f},{:.3f}) normal=({:.3f},{:.3f},{:.3f})\n",
-			bestTriV0.x, bestTriV0.y, bestTriV0.z, bestTriV1.x, bestTriV1.y, bestTriV1.z, bestTriV2.x, bestTriV2.y,
-			bestTriV2.z, bestHitNormal.x, bestHitNormal.y, bestHitNormal.z
-		);
-	}
+	//if (bestHitObj && bestHitRes && (++hitLogCounter % 30) == 0)
+	//{
+	//	std::filesystem::path meshPath;
+	//	const bool hasPath = GLOBAL(ResourceGlobal).TryGetPath(bestHitRes->GetGuid(), meshPath);
+	//	DEBUG_LOG_FMT(
+	//		"[FootIK] sample hit obj='{}' guid={} path='{}' foot=({:.3f},{:.3f},{:.3f}) ground=({:.3f},{:.3f},{:.3f}) distance={:.3f}\n",
+	//		bestHitObj->GetName().c_str(), bestHitRes->GetGuid(), hasPath ? meshPath.string() : "<unresolved>",
+	//		worldPosition.x, worldPosition.y, worldPosition.z, bestHitPoint.x, bestHitPoint.y, bestHitPoint.z,
+	//		bestRayDistance
+	//	);
+	//	DEBUG_LOG_FMT(
+	//		"[FootIK] hit tri v0=({:.3f},{:.3f},{:.3f}) v1=({:.3f},{:.3f},{:.3f}) v2=({:.3f},{:.3f},{:.3f}) normal=({:.3f},{:.3f},{:.3f})\n",
+	//		bestTriV0.x, bestTriV0.y, bestTriV0.z, bestTriV1.x, bestTriV1.y, bestTriV1.z, bestTriV2.x, bestTriV2.y,
+	//		bestTriV2.z, bestHitNormal.x, bestHitNormal.y, bestHitNormal.z
+	//	);
+	//}
 	return true;
 }
